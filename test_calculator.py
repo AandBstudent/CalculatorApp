5722270d2cba7393ee1a4bd7ffe84707f143f41f
Phillip <renaudtp@oregonstate.edu>
2021-03-09 13:30:38 -0800

    More Setup that was Required
    1. Installed dependencies: pytest, pytest-cov, and flake8(linter)
    2. Created the requirments.txt (for what idk?)
    3. Ran flake8 --statistics (got a big list of stuff)
    4. Created test_calculator.py
    5. Ran unit tests: pytest -v --cov got a dope ass report

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+"""
+Unit tests using pytest for the calculator app
+"""
+import calculator
+
+class TestCalculator:
+    
+    def test_add(self):
+        assert 6 == calculator.add(1,5)
+        
+    def test_subtract(self):
+        assert 2 == calculator.subtract(4,2)
